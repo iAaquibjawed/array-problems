9ed43b803604affe5cddd4c5aff3d50eb84238d0
iAaquibjawed <mallickaaquib@gmail.com>
2024-05-12 19:52:42 +0530

    array sorted check without sorting with little optimization

--- is-array-sorted.rb
+++ is-array-sorted.rb
@@ -19,3 +19,21 @@ end
 
 
 puts is_array_sorted([1,2,3,4,5]) # Output: True
+
+
+
+# little optimization as we are breaking and comming out of loop if we find any number in between the itteration is lesser than it's forward 
+
+
+def is_array_sorted(array)
+    is_sorted = false
+    for i in 0..array.length-2
+        if array[i+1] > array[i]
+            is_sorted = true
+        else 
+            is_sorted = false
+            break
+        end
+    end
+    return is_sorted
+end
